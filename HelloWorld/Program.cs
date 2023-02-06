eaa128b3d6f7fd8930f080318e66e8067ac1a3c3
miroslaw.szewczyk <miroslaw.szewczyk@HSPL-BAN-LP-02>
2023-02-06 19:31:39 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[])
+    }
+
+
+}
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Nazywam się Mirosław Szewczyk");
